40496b77bc0bc28a9a6f7ce932c1c3335c834deb
Milena Boselli Rosa <milena.rosa@gmail.com>
2022-02-22 20:46:29 -0300

    feat: testing lib publishing.

---
+++ src/hello-world.ts
@@ -0,0 +1,11 @@
+export function sayHello() {
+  console.log("====================================");
+  console.log("hi");
+  console.log("====================================");
+}
+
+export function sayGoodbye() {
+  console.log("====================================");
+  console.log("bye");
+  console.log("====================================");
+}
